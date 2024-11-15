d562c029808bf0d047b073b1a9ee76375a661425
santiago <santiago.viana@upb.edu.co>
2024-11-15 16:00:55 -0500

    V3

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install docker-compose -y
 sudo apt install git
 git clone https://github.com/SVA999/FinalTelematica.git
-sudo cd ProyectTelematica
+cd ProyectTelematica
 sudo docker-compose up -d
