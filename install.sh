3167e003c51fba785c9a006ff46d189718a83042
santiago <santiago.viana@upb.edu.co>
2024-11-15 19:40:19 -0500

    V6

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install docker-compose -y
 sudo apt install git
-git clone https://github.com/SVA999/FinalTelematica
-cd FinalTelematica
-sudo docker-compose up -d
+git clone https://github.com/SVA999/final-telematica
+cd final-telematica
+docker-compose up -d
--- main.tf
+++ main.tf
@@ -1,10 +1,34 @@
+terraform {
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "4.61.0"
+}
+}
+
+required_version = ">= 1.4.0"
+}
+
 provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "my_instance" {
   ami           = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.terraform_key.key_name
+  security_groups = [aws_security_group.allow_http.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              chmod +x /tmp/install.sh
+              /tmp/install.sh
+              EOF
+
+  tags = {
+    Name = "SnakeGAME"
+    Origin = "Terraform"
+  }
 }
 
 resource "aws_key_pair" "terraform_key" {
@@ -34,23 +58,6 @@ resource "aws_security_group" "allow_http" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "my_instance" {
-  ami           = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.terraform_key.key_name
-  security_groups = [aws_security_group.allow_http.name]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              chmod +x /tmp/install.sh
-              /tmp/install.sh
-              EOF
-
-  tags = {
-    Name = "SnakeGAME"
   }
 }
 
