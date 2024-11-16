8db75154c0ce636109dcd9eca198e1a0b102dd76
santiago <santiago.viana@upb.edu.co>
2024-11-15 21:55:26 -0500

    V7

--- snake-game/app.py
+++ snake-game/app.py
@@ -1,18 +1,33 @@
+# -- coding: utf-8 --
+import click
 import flask
+from flask import Flask
 
-app = flask.Flask(__name__)
+app = Flask(_name_)
 
-@app.route("/")
-def home():
-    return "este es el servidor servicios1"
+# the minimal Flask application
+@app.route('/')
+def index():
+    return '<h1>Esta pagina te recibe con un saludo!</h1>'
 
-@app.route("/api1")
-def api1():
-    return "funcion retorno api1"
 
-@app.route("/api2")
-def api2():
-    return "funcion retorno api2"
+# bind multiple URL for one view function
+@app.route('/hola')
+@app.route('/hola1')
+def say_hello():
+    return '<h1>Hola, Flask!</h1>'
+
+# dynamic route, URL variable default
+@app.route('/saludo', defaults={'name': 'Programmer'})
+@app.route('/saludo/<name>')
+def greet(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+# custom flask cli command
+@app.cli.command()
+def hello():
+    """HOLA BIENVENIDO."""
+    click.echo('Hola Humano, estoy pensando!')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
